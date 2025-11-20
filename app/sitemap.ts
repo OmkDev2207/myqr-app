b25c5b63105c28e025360bf714a9e7ae90fff53f
OMK <omkalshetti22@gmail.com>
2025-11-20 15:59:38 +0530

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://qrlabz.vercel.app/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://qrlabz.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://qrlabz.vercel.app/privacy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://qrlabz.vercel.app/terms",
+      lastModified: new Date(),
+    },
+  ];
+}
